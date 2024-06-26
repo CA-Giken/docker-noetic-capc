52bbc357aa509b17b76c7dc50106fa25144b3b73
HiraiKyo <kyo.mikhael@gmail.com>
2024-06-27 01:44:23 +0900

    Dockerfileフルスクラッチ

--- noetic/entrypoint.sh
+++ noetic/entrypoint.sh
@@ -1,2 +1,5 @@
-source /opt/ros/noetic/setup.bash
-source /app/catkin_ws/devel/setup.bash
+grep -F "source /opt/ros/noetic/setup.bash" ~/.bashrc ||
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+
+grep -F "source ~/catkin_ws/devel/setup.bash" ~/.bashrc ||
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
